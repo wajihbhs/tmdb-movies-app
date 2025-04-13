d97480d5b94cf7234f3badcb278a3f4498111e88
Wajih Bhs <wajih.bhs@doinsport.com>
2025-04-13 19:51:33 +0200

    feat(layout): create default layout and update app base structure

--- app.vue
+++ app.vue
@@ -1,6 +1,20 @@
+<script setup lang="ts">
+import { useI18n } from "vue-i18n";
+import { useHead } from "nuxt/app";
+
+const { t } = useI18n();
+
+useHead({
+  title: t("home-title"),
+});
+</script>
+
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div class="bg-gray-100 min-h-screen">
+    <NuxtLayout>
+      <main class="container mx-auto">
+        <NuxtPage />
+      </main>
+    </NuxtLayout>
   </div>
 </template>
