59fdfb66f7d7178dc7f45bd660f4946baacde24e
Wajih Bhs <wajih.bhs@doinsport.com>
2025-04-18 01:22:56 +0100

    chore: config e2e tests

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,25 +1,29 @@
-import { defineConfig } from "vitest/config";
-import { fileURLToPath } from "url";
-import { dirname } from "path";
-import path from "path"; // 👈 C'est ça qui manquait !
+import { defineConfig } from 'vitest/config'
+import vue from '@vitejs/plugin-vue'
+import path from 'path'
+import { fileURLToPath } from 'url'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 export default defineConfig({
+  plugins: [vue()],
   resolve: {
     alias: {
-      "~": path.resolve(__dirname, "./"),
-      "@": path.resolve(__dirname, "./")
-    }
+      '~': path.resolve(__dirname, './'),
+      '@': path.resolve(__dirname, './'),
+    },
   },
   test: {
     globals: true,
-    environment: "happy-dom",
-    setupFiles: ["./tests/setup.ts"],
-    include: ["**/tests/unit/**/*.test.{js,ts}"],
+    environment: 'happy-dom',
+    setupFiles: ['./tests/setup.ts'],
+    include: [
+      'tests/unit/**/*.test.{js,ts}',
+      'tests/e2e/**/*.test.{js,ts}',
+    ],
     deps: {
-      inline: ["@vue", "vue-demi"]
-    }
-  }
-});
+      inline: ['@vue', 'vue-demi'],
+    },
+  },
+})
